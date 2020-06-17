d0cf620bcae51e58b6be195bc965cb10651268db
ktleary <kevin@ktleary.com>
2020-06-17 03:26:21 -0400

    added functional connections report

--- slipbox-tag-find.py
+++ slipbox-tag-find.py
@@ -7,16 +7,40 @@ nlp = spacy.load("en_core_web_sm")
 tagfind = re.compile('#([\d\w]+)')
 tagslists = []
 tags = []
+tagsLemmasPaths = {}
+
+
+def findConnectedTags(tagdict):
+    for d in tagdict:
+        if len(tagdict[d]) > 1:
+            print(d, tagdict[d])
+
+
+def updateTagsLemmasPaths(filepath, tags):
+    tagstr = " ".join(tags)
+    tagdoc = nlp(tagstr)
+    taglemmas = lemmatize(tagdoc)
+    for taglemma in taglemmas:
+        if taglemma not in tagsLemmasPaths:
+            tagsLemmasPaths[taglemma] = [filepath]
+        else:
+            # print("Found existing tag: " + taglemma)
+            tagsLemmasPaths[taglemma].append(filepath)
+            # print(tagsLemmasPaths[taglemma])
 
 
 def findTags(filepaths):
     for filepath in filepaths:
+        filetags = []
         with open(filepath) as f:
             content = [i.strip() for i in f.readlines()]
             for line in content:
                 tags = tagfind.findall(line)
                 if tags:
                     tagslists.append(tags)
+                    for tag in tags:
+                        filetags.append(tag)
+            updateTagsLemmasPaths(filepath, filetags)
 
 
 def lemmatize(doc):
@@ -36,7 +60,9 @@ def main():
     tagset = sorted(list(set(tags)))
     doc = nlp(" ".join(tagset))
     lemmas = lemmatize(doc)
-    print(lemmas)
+    # print(lemmas)
+    # tagsLemmasPaths
+    findConnectedTags(tagsLemmasPaths)
 
 
 if __name__ == "__main__":
@@ -44,3 +70,8 @@ if __name__ == "__main__":
         sys.exit(main())
     except FileNotFoundError as e:
         sys.exit(e)
+"""
+# References
+1. mod_structured/createJsonld.py
+2. command: python slipbox-tag-find.py -d ./data -p *.md
+"""
